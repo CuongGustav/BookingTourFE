83af5d5c346515fd277740773104ff80ae60399e
CuongGustav <cuonggustav@gmail.com>
2026-01-12 21:23:31 +0700

    Read payment detail admin

--- app/types/payment.ts
+++ app/types/payment.ts
@@ -1,3 +1,5 @@
+import { PaymentImageInfo } from "./payment_image";
+
 export interface PaymentInfo{
     payment_id: string,
     booking_id: string,
@@ -6,4 +8,15 @@ export interface PaymentInfo{
     amount: number,
     status: "PENDING" | "COMPLETED" | "FAILED" | "REFUNDED",
     created_at: string,
+}
+
+export interface ReadPaymentAdminInfo {
+    payment_id: string,
+    booking_id: string,
+    booking_code: string,
+    payment_method: "CASH" | "BANK_TRANSFER",
+    amount: number,
+    status: "PENDING" | "COMPLETED" | "FAILED" | "REFUNDED",
+    created_at: string,
+    payment_images: PaymentImageInfo[],
 }
