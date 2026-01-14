e893d172bba07085decf8a9fe6bbbe99589c7aee
CuongGustav <cuonggustav@gmail.com>
2026-01-14 20:50:45 +0700

    Read list review user

---
+++ app/types/reviews.ts
@@ -0,0 +1,10 @@
+export default interface ReviewInfo {
+    account_id: string,
+    booking_code: string,
+    booking_id: string,
+    comment: string,
+    created_at: string,
+    rating: number,
+    review_id: string,
+    tour_id: string,
+}
